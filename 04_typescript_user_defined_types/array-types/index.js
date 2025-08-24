8c7cc155c3807e46f11c9703f344d8cc7d25bd9c
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-24 19:58:22 +0600

    typescript array types

---
+++ 04_typescript_user_defined_types/array-types/index.js
@@ -0,0 +1,8 @@
+// string array:
+var fruits;
+fruits = ['apple', 'mango', 'jackfruit'];
+console.log(fruits);
+// mixed array:
+var user;
+user = ['Rahman', 100];
+console.log(user);
---
+++ 04_typescript_user_defined_types/array-types/index.ts
@@ -0,0 +1,12 @@
+// string array:
+
+let fruits: string[];
+fruits = ['apple', 'mango', 'jackfruit'];
+
+console.log(fruits);
+
+// mixed array:
+let user: (string | number)[];
+user = ['Rahman', 100];
+
+console.log(user);
