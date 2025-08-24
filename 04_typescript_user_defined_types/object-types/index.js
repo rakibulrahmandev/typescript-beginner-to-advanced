9c7a99e38cd612eaac72b2501538d807a7db8434
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-08-24 21:20:08 +0600

    typescript object types

---
+++ 04_typescript_user_defined_types/object-types/index.js
@@ -0,0 +1,12 @@
+var user1;
+user1 = {
+    username: 'Rakibul Rahman',
+    userid: 18,
+};
+console.log(user1);
+var user2;
+user2 = [{ username: 'Rahman', userid: 18 }, { username: 'Ensiya', userid: 15 }];
+console.log(user2);
+var user3;
+user3 = { username: 'Rakibul Rahman', userid: 18 };
+console.log(user3);
---
+++ 04_typescript_user_defined_types/object-types/index.ts
@@ -0,0 +1,17 @@
+let user1: object;
+user1 = {
+    username: 'Rakibul Rahman',
+    userid: 18, 
+};
+
+console.log(user1);
+
+let user2: object[];
+user2 = [{username: 'Rahman', userid: 18}, {username: 'Ensiya', userid: 15}];
+
+console.log(user2);
+
+let user3: {username: string, userid: number};
+user3 = {username: 'Rakibul Rahman', userid: 18};
+
+console.log(user3);
